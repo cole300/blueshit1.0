2d559f649d3764f2abc5cf6ad7bfb54d7a5b901a
cole300 <306913419@qq.com>
2016-03-08 18:59:56 +0800

    test style

--- root/index.php
+++ root/index.php
@@ -1,4 +1,22 @@
-<php?
-	echo();
-	echo "test";
-?>
+<!DOCTYPE html>
+<html>
+	<head> 
+		<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+		<title> BlueShit Distribution Tools </title>
+		<link rel="stylesheet" href="comm.css" type="text/css" />
+	</head>
+	<body bgcolor="#EEEEEE">
+		<br/>
+		<center>
+			<h1> <span style='color:#556B2F;'>测试欢迎页面</span> </h1>
+		<br/>
+
+		<h3>
+			<form method='POST' enctype='multipart/form-data' action='/resign'>
+				上传IPA包转企业签名包: 
+				<input type="file"  class="input-file" name="ipafile">
+				<input type="submit" class="btn btn-default" />
+			</form>
+		</h3>
+	</body>
+</html>
